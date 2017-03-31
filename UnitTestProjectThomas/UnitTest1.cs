10a6222858cefd323c2d5bcafdc0335a7be36faf
TCorrege <thomas.correge@live.fr>
2017-03-31 10:44:03 +0200

    test1

--- UnitTestProjectThomas/UnitTest1.cs
+++ UnitTestProjectThomas/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
 
 namespace UnitTestProjectThomas
 {
@@ -9,6 +10,16 @@ namespace UnitTestProjectThomas
         [TestMethod]
         public void TestMethod1()
         {
+            homme UnHomme1 = new homme(1, "courtine", "jerome", new DateTime(1968, 1, 18));
+            homme UnHomme2 = new homme(1, "correge", "thomas", new DateTime(2015, 1, 18));
+            femme UneFemme1 = new femme(3, "mothe", "josianne", new DateTime(1967, 4, 1), 100);
+            Assert.AreEqual(UnHomme1.EstMajeur(), true);
+            Assert.AreEqual(UnHomme2.EstMajeur(), false);
+            tribu Matribu = new tribu();
+            Matribu.AjouterMembre(UnHomme1);
+            Matribu.AjouterMembre(UnHomme2);
+            Matribu.AjouterMembre(UneFemme1);
+            Assert.AreEqual(Matribu.NBFemmes(), 1);
         }
     }
 }
