7450f06ec1f93abe8e0f16931bb23fb1abe257ef
TCorrege <thomas.correge@live.fr>
2017-03-31 09:21:14 +0200

    Ajoutez des fichiers projet.

---
+++ App_con8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_con8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("coucou");
+            Console.WriteLine("Hello ");
+            String s = Console.ReadLine();
+            Console.WriteLine(s);
+            s = Console.ReadLine();
+        }
+    }
+}
